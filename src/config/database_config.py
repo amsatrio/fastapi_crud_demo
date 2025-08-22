d1a4f11d7ac80d57251f3817e6f3fe5a09199545
amsatrio <79563756+amsatrio@users.noreply.github.com>
2025-08-22 14:40:53 +0700

    update declarative base

--- src/config/database_config.py
+++ src/config/database_config.py
@@ -1,9 +1,8 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.future import select
 from src.config import configuration
-from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
