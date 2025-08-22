92995d583de5f9d66dc998f24a5d21547e49ea19
amsatrio <79563756+amsatrio@users.noreply.github.com>
2025-08-22 16:24:02 +0700

    update deps

--- src/config/database_config.py
+++ src/config/database_config.py
@@ -1,5 +1,6 @@
+from typing import AsyncGenerator
 from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.future import select
 from src.config import configuration
@@ -12,13 +13,17 @@ DATABASE_URL = configuration.get_settings().database_url
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 # Create a session local class
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+SessionLocal = async_sessionmaker(
+        autocommit=False,
+        autoflush=False,
+        bind=engine,
+        expire_on_commit=False, # Often set to False for async sessions
+    )
 
 # Dependency
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with SessionLocal() as session:
         yield session
-
 
 def create_table_if_not_exist():
     Base.metadata.create_all(bind=engine)
