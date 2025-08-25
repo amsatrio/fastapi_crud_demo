cb1b45d656217e2b18bb07c4a700226679162992
amsatrio <79563756+amsatrio@users.noreply.github.com>
2025-08-25 11:11:49 +0700

    add function to delete a file

--- src/util/file_util.py
+++ src/util/file_util.py
@@ -32,3 +32,10 @@ def write_file(file, base_path, file_id, file_type, file_name):
     finally:
         file.close()
     return None
+
+def delete_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+        print(f"File '{file_path}' deleted successfully.")
+    else:
+        print(f"File '{file_path}' not found.")
